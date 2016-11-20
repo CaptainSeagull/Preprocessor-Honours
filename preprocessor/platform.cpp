df39e1ee204ac8559e1b5125d8202dc58085a0e4
Jonathan Livingstone <seagull127@ymail.com>
2016-11-20 15:35:50 +0000

    Tidying.

--- preprocessor/platform.cpp
+++ preprocessor/platform.cpp
@@ -32,8 +32,7 @@ read_entire_file_and_null_terminate(Char *filename, Memory *memory)
         fseek(file, 0, SEEK_SET);
 
         res = push_file_memory(memory, size + 1);
-        PtrSize bytes_read = fread(res, 1, size, file);
-        //assert(bytes_read == size); // TODO(Jonny): Why does this fail on Windows??
+        fread(res, 1, size, file);
         fclose(file);
     }
 
