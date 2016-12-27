60723454e3b4cfe12bed3ba27423d8f8a61ae0d7
Jonathan Livingstone <seagull127@ymail.com>
2016-12-27 06:31:04 +0000

    Just tidying the Linux build file a little more.

--- build_clang.sh
+++ build_clang.sh
@@ -1,40 +1,48 @@
 # Batch File to compile the preprocessor on Linux.
 
-RELEASE=true
-RUN_CODE_AFTER_BUILDING=true
-RUN_TEST=true
-RUN_GCC_FOR_EXTRA_TEST=true
+RELEASE=false
+GTEST=true
+RUN_TEST_CODE=true
+RUN_GCC_FOR_EXTRA_TEST=false
 
 # Preprocessor
 pushd "preprocessor"
 
-COMMON_WARNINGS="-Wno-unused-function -Wno-c++11-compat-deprecated-writable-strings -Wno-switch -Wno-sign-compare -Wno-unused-parameter"
+COMMON_WARNINGS_CLANG="-Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -Wno-switch -Wno-sign-compare -Wno-unused-parameter"
 
-if [ "$RELEASE" == "$true" ]; then
-    clang++ -Wall "preprocessor.cpp" -std=c++11 -o preprocessor -DERROR_LOGGING=0 -DRUN_TESTS=0 -DINTERNAL=0 -DMEM_CHECK=0 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS -ldl 
+if [ "$RELEASE" == "true" ]; then
+    clang++ -Wall -Wextra "preprocessor.cpp" -std=c++11 -o preprocessor -DERROR_LOGGING=0 -DRUN_TESTS=0 -DINTERNAL=0 -DMEM_CHECK=0 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS_CLANG -ldl
 else
-    clang++ -Wall -Wextra "preprocessor.cpp" -std=c++11 -o preprocessor -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS -g -ldl -pthread
+    if [ "$GTEST" == "true" ]; then
+        clang++ -Wall -Wextra "preprocessor.cpp" "google_test/gtest-all.cc" -std=c++11 -o preprocessor -DERROR_LOGGING=1 -DRUN_TESTS=1 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS_CLANG -g -ldl -pthread
+    else
+        clang++ -Wall -Wextra "preprocessor.cpp" -std=c++11 -o preprocessor -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS_CLANG -g -ldl -pthread
+    fi
 fi
 mv "./preprocessor" "../builds/linux_clang/preprocessor"
 
 # GCC
-if [ "$RELEASE" == "$true" ]; then
-    COMMON_WARNINGS="-Wno-write-strings -Wno-switch -Wno-sign-compare -Wno-missing-field-initializers -Wno-unused-parameter"
-    gcc -Wall -Wextra "preprocessor.cpp" -std=c++11 -o preprocessor_gcc -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS -g -ldl -pthread
+if [ "$RUN_GCC_FOR_EXTRA_TEST" == "true" ]; then
+    COMMON_WARNINGS_GCC="-Wno-write-strings -Wno-switch -Wno-sign-compare -Wno-missing-field-initializers -Wno-unused-parameter"
+    gcc -Wall -Wextra "preprocessor.cpp" -std=c++11 -o preprocessor_gcc -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $COMMON_WARNINGS_GCC -g -ldl -pthread
     rm "./preprocessor_gcc"
 fi
 
 popd
 
 # Run code after building.
-if [ "$RUN_CODE_AFTER_BUILDING%"=="true" ]; then
-    ./builds/linux_clang/preprocessor -t
+if [ "$GTEST" == "true" ]; then
+    if [ "$RELEASE" == "false" ]; then      
+        ./builds/linux_clang/preprocessor -t
+    fi
 fi
 
 # Test.
-pushd "test"
-"../builds/linux_clang/preprocessor" test_code.cpp
+if [ "$RUN_TEST_CODE" == "true" ]; then
+    pushd "test"
+    "../builds/linux_clang/preprocessor" test_code.cpp
 
-clang++ -Wall "test_code.cpp" -std=c++11 -o text_exe -Wno-unused-function -Wno-unused-variable -g -ldl
-mv "./text_exe" "../builds/linux_clang/text"
+    clang++ -Wall "test_code.cpp" -std=c++11 -o text_exe -Wno-unused-function -Wno-unused-variable -g -ldl
+    mv "./text_exe" "../builds/linux_clang/text"
+fi
 
