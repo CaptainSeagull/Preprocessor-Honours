7986b86ed55ae4e813d10c99a935e79b282675b0
Jonathan Livingstone <seagull127@ymail.com>
2017-01-02 15:07:49 +0000

    Tidying.

--- win32_build.bat
+++ win32_build.bat
@@ -1,13 +1,12 @@
 @echo off
 
 rem Variables to set. NOTE - Google Test uses _a lot_ of memory, so it's advised to run tests in 64-bit.
-set VISUAL_STUDIO_VERSION=10
-set ENVIRONMENT=x64
+set VISUAL_STUDIO_VERSION=12
+set ENVIRONMENT=x86
 set RELEASE=false
-set RUN_CODE_AFTER_BUILDING=false
+set RUN_CODE_AFTER_BUILDING=true
 
-set RUN_SDL_CODE=false
-set RUN_TEST=false
+set RUN_TEST=true
 set RUN_LAUREN_TEST=false
 
 rem Warnings to ignore.
@@ -19,51 +18,39 @@ call "C:\Program Files (x86)\Microsoft Visual Studio %VISUAL_STUDIO_VERSION%.0\V
 set COMMON_COMPILER_FLAGS=-nologo -MTd -Gm- -GR- -EHsc- -Od -Oi %COMMON_WARNINGS% -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=0 -DWIN32=1 -DLINUX=0 -FC -Zi -GS- -Gs9999999
 
 setlocal EnableDelayedExpansion
-set FILES="../../preprocessor/preprocessor.cpp" "../../preprocessor/google_test/gtest-all.cc"
+set FILES="../preprocessor/preprocessor.cpp" "../preprocessor/google_test/gtest-all.cc"
 if "%RELEASE%"=="true" (
     set COMMON_COMPILER_FLAGS=-nologo -MT -fp:fast -Gm- -GR- -EHa- -O2 -Oi %COMMON_WARNINGS% -DINTERNAL=0 -DWIN32=1 -DLINUX=0 -FC -Zi -GS- -Gs9999999
-    set FILES="../../preprocessor/preprocessor.cpp"
+    set FILES="../preprocessor/preprocessor.cpp"
 )
 
-IF NOT EXIST "builds/win32_msvc" mkdir "builds/win32_msvc"
-pushd "builds/win32_msvc"
+IF NOT EXIST "build" mkdir "build"
+pushd "build"
 
 cl -FePreprocessor %COMMON_COMPILER_FLAGS% -Wall %FILES% -link -subsystem:console,5.2 kernel32.lib
 popd
 
 if "%RUN_CODE_AFTER_BUILDING%"=="true" (
-    "builds/win32_msvc/preprocessor.exe" -t
-)
-
-if "%RUN_SDL_CODE%"=="true" (
-    if "%ENVIRONMENT%"=="x86" (
-        pushd "app test"
-        "../builds/win32_msvc/preprocessor.exe" sdl_main.cpp sdl_other.cpp
-        popd
-        
-        pushd "builds/win32_msvc"
-        cl -FeTestSDL %COMMON_COMPILER_FLAGS% -wd4098 -Wall "../../app test/sdl_main.cpp" "../../app test/sdl_other.cpp" -FmTest.map -link -subsystem:windows,5.2 kernel32.lib SDL2.lib SDL2main.lib
-        popd
-    )
+    "build/preprocessor.exe" -t
 )
 
 if "%RUN_LAUREN_TEST%"=="true" (
     pushd "lauren_test"
-    "../builds/win32_msvc/preprocessor.exe" test.cpp
+    "../build/preprocessor.exe" test.cpp
     popd
 
-    pushd "builds/win32_msvc"
-    cl -FeTestLauren %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../../lauren_test/test.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
+    pushd "build"
+    cl -FeTestLauren %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../lauren_test/test.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
     popd
 )
 
 
 if "%RUN_TEST%"=="true" (
     pushd "test"
-    "../builds/win32_msvc/preprocessor.exe" test_code.cpp -p
+    "../build/preprocessor.exe" test_code.cpp -p
     popd
 
-    pushd "builds/win32_msvc"
-    cl -FeTestCode %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../../test/test_code.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
+    pushd "build"
+    cl -FeTestCode %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../test/test_code.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
     popd
 )
