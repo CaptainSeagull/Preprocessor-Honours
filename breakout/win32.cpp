46910d2664b4beb444fc8c161d150d6a87ec6b06
Jonathan Livingstone <seagull127@ymail.com>
2017-01-03 18:45:44 +0000

    Started a breakout example.

---
+++ breakout/win32.cpp
@@ -0,0 +1,300 @@
+#include <windows.h>
+#include <dsound.h>
+#include <gl/gl.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
+
+static bool global_running = true;
+static bool global_init_game = true;
+static HDC global_dc = 0;
+
+#define SCREEN_WIDTH (640)
+#define SCREEN_HEIGHT (480)
+
+static bool wini32_init_gl(HWND wnd) {
+    bool res = false;
+
+    PIXELFORMATDESCRIPTOR desired_pixel_format = {};
+    desired_pixel_format.nSize = sizeof(desired_pixel_format);
+    desired_pixel_format.nVersion = 1;
+    desired_pixel_format.iPixelType = PFD_TYPE_RGBA;
+    desired_pixel_format.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    desired_pixel_format.cColorBits = 24; // TODO(Jonny): Should this be 32... even though MSDN says EXCLUDING alpha??
+    desired_pixel_format.cAlphaBits = 8;
+
+    int suggested_pixel_format_index = ChoosePixelFormat(global_dc, &desired_pixel_format);
+    PIXELFORMATDESCRIPTOR suggested_pixel_format = {};
+    DescribePixelFormat(global_dc, suggested_pixel_format_index, sizeof(suggested_pixel_format), &suggested_pixel_format);
+    SetPixelFormat(global_dc, suggested_pixel_format_index, &suggested_pixel_format);
+
+    HGLRC glrc = wglCreateContext(global_dc);
+    if(glrc) {
+        if(wglMakeCurrent(global_dc, glrc)) {
+            res = true;
+
+#if 0
+#define WGL_CONTEXT_MAJOR_VERSION_ARB (0x2091)
+#define WGL_CONTEXT_MINOR_VERSION_ARB (0x2092)
+#define WGL_CONTEXT_FLAGS_ARB (0x2094)
+#define WGL_CONTEXT_PROFILE_MASK_ARB (0x9126)
+#define WGL_CONTEXT_DEBUG_BIT_ARB (0x0001)
+#define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB (0x00000002)
+
+            typedef HGLRC WINAPI wgl_create_context_attribts_arb(HDC hDC, HGLRC hShareContext, const int *attribList);
+            wgl_create_context_attribts_arb *wglCreateContextAttribsARB =
+                (wgl_create_context_attribts_arb *)wglGetProcAddress("wglCreateContextAttribsARB");
+            if(!wglCreateContextAttribsARB) { fprintf(stderr, "Failed to load wglCreateContextAttribsARB."); }
+            else {
+                int attribs[] = {WGL_CONTEXT_MAJOR_VERSION_ARB, 3, WGL_CONTEXT_MINOR_VERSION_ARB, 0, WGL_CONTEXT_FLAGS_ARB,
+#if INTERNAL
+                                 WGL_CONTEXT_DEBUG_BIT_ARB
+#else
+                                 0
+#endif
+                                 , WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB, 0
+                                };
+                HGLRC shared_context = 0;
+                HGLRC new_glrc = wglCreateContextAttribsARB(dc, shared_context, attribs);
+                if(new_glrc) {
+                    if(wglMakeCurrent(dc, new_glrc)) {
+                        wglDeleteContext(glrc);
+                        glrc = new_glrc;
+
+                        typedef int WINAPI wgl_swap_interval_ext(int interval);
+                        wgl_swap_interval_ext *wglSwapIntervalEXT =
+                            (wgl_swap_interval_ext *)wglGetProcAddress("wglSwapIntervalEXT");
+                        if(!wglSwapIntervalEXT) { fprintf(stderr, "Failed to load wglSwapIntervalEXT"); }
+                        else {
+                            wglSwapIntervalEXT(1);
+                            res = true;
+                        }
+                    }
+                }
+            }
+#endif
+        }
+    }
+
+    return(res);
+}
+
+static LRESULT CALLBACK Win32WinProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    LRESULT res = 0;
+    switch(msg) {
+        case WM_CLOSE: { global_running = false;                         } break;
+        default:       { res = DefWindowProcA(wnd, msg, wparam, lparam); } break;
+    }
+
+    return(res);
+};
+
+static LARGE_INTEGER Win32GetWallClock(void) {
+    LARGE_INTEGER res;
+    QueryPerformanceCounter(&res);
+
+    return(res);
+}
+
+static float Win32GetSecondsElapsed(LARGE_INTEGER start, LARGE_INTEGER end, int64_t perf_counter_frequency) {
+    return((float)(end.QuadPart - start.QuadPart) / (float)perf_counter_frequency);
+}
+
+struct Texture {
+    int unsigned id;
+    int width;
+    int height;
+};
+
+Texture load_texture_from_disk(char const *fname) {
+    Texture res = {};
+
+    // Create texture.
+    int channels = 0;
+    unsigned char *data = stbi_load(fname, &res.width, &res.height, &channels, 4);
+    if(channels != 4) { fprintf(stderr, "Can only load png files with 4 channels"); }
+    else {
+        // Generate, and bind, texture ID.
+        glGenTextures(1, &res.id);
+        glBindTexture(GL_TEXTURE_2D, res.id);
+
+        // Generate texture.
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, res.width, res.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        // Set texture parameters.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        // Now unbind the texture.
+        glBindTexture(GL_TEXTURE_2D, NULL);
+    }
+
+    return(res);
+}
+
+void delete_texture(Texture t) { glDeleteTextures(1, &t.id); }
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR CommandLine, int ShowCode) {
+    LARGE_INTEGER perf_counter_frequency_res;
+    QueryPerformanceFrequency(&perf_counter_frequency_res);
+    int64_t perf_counter_frequency = perf_counter_frequency_res.QuadPart;
+
+    WNDCLASS wnd_class = {};
+    wnd_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wnd_class.hInstance = Instance;
+    wnd_class.lpszClassName = TEXT("Frozen_Game");
+    wnd_class.lpfnWndProc = &Win32WinProc;
+
+    int monitor_refresh_hz = 60;
+    int game_update_hz = monitor_refresh_hz;
+    float target_seconds_per_frame = 1.0f / (float)game_update_hz;
+    float target_ms = (1.0f / monitor_refresh_hz) * 1000.0f;
+
+    timeBeginPeriod(1);
+
+    if(!RegisterClassA(&wnd_class)) { fprintf(stderr, "Failed to register window class (somehow??)"); }
+    else {
+        HWND wnd = CreateWindowExA(0, wnd_class.lpszClassName, TEXT("Breakout"), (WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_POPUP),
+                                   CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, 0, 0, Instance, 0);
+        if(wnd == INVALID_HANDLE_VALUE) { fprintf(stderr, "Failed to create a wnd."); }
+        else {
+            global_dc = GetDC(wnd);
+            if(!global_dc) { fprintf(stderr, "Failed to get a DC for the window."); }
+            else {
+                if(!wini32_init_gl(wnd)) { fprintf(stderr, "Failed to load OpenGL"); }
+                else {
+                    // Enable opengl stuff.
+                    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+                    glMatrixMode(GL_PROJECTION);
+                    glLoadIdentity();
+                    glOrtho(0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f, 1.0f, -1.0f);
+                    glMatrixMode(GL_MODELVIEW);
+                    glLoadIdentity();
+                    glPushMatrix();
+                    glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Default to red.
+                    glEnable(GL_TEXTURE_2D);
+                    glEnable(GL_BLEND);
+                    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+                    if(glGetError() != GL_NO_ERROR) { fprintf(stderr, "Error initializing OpenGL!\n"); }
+                    else {
+                        LARGE_INTEGER last_counter = Win32GetWallClock();
+                        LARGE_INTEGER flip_wall_clock = Win32GetWallClock();
+
+                        float ms_per_frame = 16.6666f;
+                        global_running = true;
+                        global_init_game = true;
+                        bool SoundIsValid = false;
+
+                        float camx = 0.0f, camy = 0.0f;
+
+                        Texture tree = load_texture_from_disk("tree.png");
+                        Texture ball = load_texture_from_disk("ball.png");
+
+                        // Game loop start.
+                        while(global_running) {
+                            MSG msg;
+                            while(PeekMessageA(&msg, wnd, 0, 0, PM_REMOVE)) {
+                                switch(msg.message) {
+                                    case WM_QUIT: { global_running = false; } break;
+
+                                    case WM_KEYDOWN: {
+                                        switch(msg.wParam) {
+                                            case 'a': case VK_LEFT:  { camx += 20.0f; } break;
+                                            case 'd': case VK_RIGHT: { camx -= 20.0f; } break;
+                                            case 'w': case VK_UP:    { camy += 20.0f; } break;
+                                            case 's': case VK_DOWN:  { camy -= 20.0f; } break;
+                                        }
+                                    } break;
+
+                                    default: {
+                                        TranslateMessage(&msg);
+                                        DispatchMessageA(&msg);
+                                    } break;
+                                }
+                            }
+
+                            // TODO(Jonny): Do game stuff here.
+
+                            glMatrixMode(GL_MODELVIEW);
+                            glPopMatrix();
+                            glLoadIdentity();
+
+                            glTranslatef(camx, camy, 0.0f);
+                            glPushMatrix();
+
+                            glClear(GL_COLOR_BUFFER_BIT);
+                            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+                            glMatrixMode(GL_MODELVIEW);
+                            glPopMatrix();
+
+                            // Drawing test.
+#if 1
+                            {
+                                glScalef(0.25f, 0.25f, 1.0f);
+
+                                float x = 50.0f, y = 50.0f;
+
+                                glBindTexture(GL_TEXTURE_2D, ball.id);
+                                glColor3f(1.0f, 1.0f, 1.0f);
+                                glBegin(GL_POLYGON);
+                                glTexCoord2f(0.0f, 0.0f); glVertex2f(x + 0,         y + 0);
+                                glTexCoord2f(1.0f, 0.0f); glVertex2f(x + ball.width, y + 0);
+                                glTexCoord2f(1.0f, 1.0f); glVertex2f(x + ball.width, y + ball.height);
+                                glTexCoord2f(0.0f, 1.0f); glVertex2f(x + 0,         y + ball.height);
+                                glEnd();
+
+                                glBindTexture(GL_TEXTURE_2D, 0);
+
+                                glScalef(2.0f, 2.0f, 1.0f);
+
+                                x = 250.0f, y = 250.0f;
+                                glBindTexture(GL_TEXTURE_2D, tree.id);
+                                glColor3f(1.0f, 1.0f, 1.0f);
+                                glBegin(GL_POLYGON);
+                                glTexCoord2f(0.0f, 0.0f); glVertex2f(x + 0,         y + 0);
+                                glTexCoord2f(1.0f, 0.0f); glVertex2f(x + tree.width, y + 0);
+                                glTexCoord2f(1.0f, 1.0f); glVertex2f(x + tree.width, y + tree.height);
+                                glTexCoord2f(0.0f, 1.0f); glVertex2f(x + 0,         y + tree.height);
+                                glEnd();
+                                glBindTexture(GL_TEXTURE_2D, 0);
+                            }
+#endif
+
+                            SwapBuffers(global_dc);
+
+                            float seconds_for_frame = Win32GetSecondsElapsed(last_counter, Win32GetWallClock(), perf_counter_frequency);
+                            if(seconds_for_frame >= target_seconds_per_frame) { fprintf(stderr, "Missed frame rate"); }
+                            else {
+                                DWORD time_to_sleep = (DWORD)(1000.0f * (target_seconds_per_frame - seconds_for_frame));
+                                if(time_to_sleep > 0) { Sleep(time_to_sleep); }
+
+                                float test_seconds_elapsed_for_frame = Win32GetSecondsElapsed(last_counter, Win32GetWallClock(),
+                                                                                              perf_counter_frequency);
+                                if(test_seconds_elapsed_for_frame < target_seconds_per_frame) { fprintf(stderr, "Missed sleep"); }
+
+                                while(seconds_for_frame < target_seconds_per_frame) {
+                                    seconds_for_frame = Win32GetSecondsElapsed(last_counter, Win32GetWallClock(),
+                                                                               perf_counter_frequency);
+                                }
+                            }
+
+                            LARGE_INTEGER end_counter = Win32GetWallClock();
+                            ms_per_frame = 1000.0f * Win32GetSecondsElapsed(last_counter, end_counter, perf_counter_frequency);
+                            last_counter = end_counter;
+
+                            flip_wall_clock = Win32GetWallClock();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return(0);
+}
--- win32_build.bat
+++ win32_build.bat
@@ -1,16 +1,16 @@
 @echo off
 
 rem Variables to set. NOTE - Google Test uses _a lot_ of memory, so it's advised to run tests in 64-bit.
-set VISUAL_STUDIO_VERSION=12
+set VISUAL_STUDIO_VERSION=10
 set ENVIRONMENT=x86
 set RELEASE=false
 set RUN_CODE_AFTER_BUILDING=true
 
-set RUN_TEST=true
-set RUN_LAUREN_TEST=false
+set RUN_TEST=false
+set RUN_BREAKOUT=true
 
 rem Warnings to ignore.
-set COMMON_WARNINGS=-wd4189 -wd4706 -wd4996 -wd4100 -wd4127 -wd4267 -wd4505 -wd4820 -wd4365 -wd4514 -wd4062 -wd4061 -wd4668 -wd4389 -wd4018 -wd4711 -wd4987 -wd4710 -wd4625 -wd4626 -wd4350 -wd4826 -wd4640 -wd4571 -wd4986 -wd4388
+set COMMON_WARNINGS=-wd4189 -wd4706 -wd4996 -wd4100 -wd4127 -wd4267 -wd4505 -wd4820 -wd4365 -wd4514 -wd4062 -wd4061 -wd4668 -wd4389 -wd4018 -wd4711 -wd4987 -wd4710 -wd4625 -wd4626 -wd4350 -wd4826 -wd4640 -wd4571 -wd4986 -wd4388 -wd4129
  
 rem 32/64 bit builds.
 call "C:\Program Files (x86)\Microsoft Visual Studio %VISUAL_STUDIO_VERSION%.0\VC\vcvarsall.bat" %ENVIRONMENT%
@@ -34,23 +34,22 @@ if "%RUN_CODE_AFTER_BUILDING%"=="true" (
     "build/preprocessor.exe" -t
 )
 
-if "%RUN_LAUREN_TEST%"=="true" (
-    pushd "lauren_test"
-    "../build/preprocessor.exe" test.cpp
-    popd
-
-    pushd "build"
-    cl -FeTestLauren %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../lauren_test/test.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
-    popd
-)
-
-
 if "%RUN_TEST%"=="true" (
     pushd "test"
     "../build/preprocessor.exe" test_code.cpp -p
     popd
 
     pushd "build"
-    cl -FeTestCode %COMMON_COMPILER_FLAGS% -wd4098 -W1 "../test/test_code.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
+    cl -FeTestCode %COMMON_COMPILER_FLAGS% -Wall "../test/test_code.cpp" -FmTest.map -link -subsystem:console,5.2 kernel32.lib
+    popd
+)
+
+if "%RUN_BREAKOUT%"=="true" (
+    rem pushd "breakout"
+    rem "../build/preprocessor.exe" test_code.cpp -p
+    rem popd
+
+    pushd "build"
+    cl -FeBreakout %COMMON_COMPILER_FLAGS% -wd4201 -Wall "../breakout/win32.cpp" -FmTest.map -link -subsystem:windows,5.2 kernel32.lib user32.lib gdi32.lib opengl32.lib winmm.lib dsound.lib
     popd
 )
