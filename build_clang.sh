670db278fc5b39bbd3d4ed2c55c4501900778618
Jonathan Livingstone <seagull127@ymail.com>
2017-02-02 09:38:50 +0000

    Made it easier to change what version of Clang is being used, similar to MSVC.

--- build_clang.sh
+++ build_clang.sh
@@ -4,6 +4,7 @@ RELEASE=false
 GTEST=false
 BUILD_TEST_CODE=true
 BUILD_BREAKOUT=true
+CLANG_VERSION=3.8
 
 # Preprocessor
 WARNINGS="-Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -Wno-switch -Wno-sign-compare -Wno-unused-parameter -Wno-writable-strings -Wno-unknown-escape-sequence"
@@ -15,9 +16,9 @@ if [ "$RELEASE" = "true" ]; then
     clang++-3.8 -Wall -Wextra $FILES -std=c++98 -fno-exceptions -fno-rtti -o preprocessor_exe -DERROR_LOGGING=0 -DRUN_TESTS=0 -DINTERNAL=0 -DMEM_CHECK=0 -DWIN32=0 -DLINUX=1 $WARNINGS -g -ldl
 else
     if [ "$GTEST" = "false" ]; then
-        clang++-3.8 -Wall -Wextra $FILES -std=c++98 -fno-exceptions -fno-rtti -o preprocessor_exe -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $WARNINGS -g -ldl
+        clang++-"$CLANG_VERSION" -Wall -Wextra $FILES -std=c++98 -fno-exceptions -fno-rtti -o preprocessor_exe -DERROR_LOGGING=1 -DRUN_TESTS=0 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $WARNINGS -g -ldl
     else
-        clang++-3.8 -Wall -Wextra $FILES "preprocessor/test.cpp" "preprocessor/google_test/gtest-all.cc" -std=c++98 -fno-exceptions -fno-rtti -o preprocessor_exe -DERROR_LOGGING=1 -DRUN_TESTS=1 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $WARNINGS -g -ldl -pthread
+        clang++-"$CLANG_VERSION" -Wall -Wextra $FILES "preprocessor/test.cpp" "preprocessor/google_test/gtest-all.cc" -std=c++98 -fno-exceptions -fno-rtti -o preprocessor_exe -DERROR_LOGGING=1 -DRUN_TESTS=1 -DINTERNAL=1 -DMEM_CHECK=1 -DWIN32=0 -DLINUX=1 $WARNINGS -g -ldl -pthread
     fi
 fi
 mv "./preprocessor_exe" "build/preprocessor"
@@ -37,7 +38,7 @@ if [ "$BUILD_TEST_CODE" = "true" ]; then
     "../build/preprocessor" test_code.cpp
     popd
 
-    clang++-3.8 -Wall -Wextra "test/test_code.cpp" -std=c++11 -o test_exe -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -g -ldl
+    clang++-"$CLANG_VERSION" -Wall -Wextra "test/test_code.cpp" -std=c++11 -o test_exe -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -g -ldl
     mv "./test_exe" "build/test"
 fi
 
@@ -48,6 +49,6 @@ if [ "$BUILD_BREAKOUT" = "true" ]; then
     "../build/preprocessor" breakout.cpp
     popd
 
-    clang++-3.8 -Wall -Wextra "breakout/breakout.cpp" -std=c++11 -o breakout_exe -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -g -ldl -lSDL2
+    clang++-"$CLANG_VERSION" -Wall -Wextra "breakout/breakout.cpp" -std=c++11 -o breakout_exe -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-c++11-compat-deprecated-writable-strings -g -ldl -lSDL2
     mv "./breakout_exe" "build/breakout"
 fi
