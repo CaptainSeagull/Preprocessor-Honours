14ddda6f5fbb06b78537ff5cb128630c096c3c13
Jonathan Livingstone <seagull127@ymail.com>
2017-03-05 11:35:30 +0000

    Was playing around with test code.

--- test/test_code.cpp
+++ test/test_code.cpp
@@ -10,6 +10,7 @@
   ===================================================================================================*/
 
 // File: test_code.cpp
+#if 0
 #include "pp_generated/test_code_generated.h"
 
 //
@@ -182,114 +183,110 @@ struct StringTest {
     int len;
     int some_array[5];
 };
+#endif
+
+#if 0
+struct C {int i;};
+struct B : public C {int i;};
+struct A : public B {int i;};
+
+class BaseClass {
+public:
+    float x;
+    float y;
+    float z;
+};
+
+class SomeClass : public BaseClass {
+public:
+    int a;
+    int b;
+    int c;
+};
+
+#endif
+
+class Struct {
+    char const *text;
+    int length;
+};
+
+#include "pp_generated/test_code_generated.h"
+#include <iostream>
+
+class Vector2d {
+public:
+    int x, y;
+
+    void operator++() {
+        ++x; ++y;
+    }
+};
+
+class Test {
+public:
+    int a;
+    int b;
+    float c;
+    Vector2d d;
+};
+
+struct Foo {};
 
 
-int main(int /*argc*/, char ** /*argv*/) {
-    //test_struct();
-    //test_enum();
-    //std_test();
+int main(int argc, char **argv) {
+    Test test;
+    test.a = 1;
+    test.b = 2;
+    test.c = 3.5f;
+    test.d = {4, 5};
 
-
-    //
-    //
-    //
-
-    StringTest test;// = {"Hello", 5};
-    test.text = "Hello";
-    test.len = 5;
-    for(int i = 0; (i < 5); ++i) test.some_array[i] = 5 - i;
-    test.ptr = new float; *test.ptr = 3.14f;
-
+    std::cout << "Before";
     pp::print(test);
 
+    for(int i = 0; (i < pp::TypeInfo<decltype(test)>::member_count); ++i) {
+        pp::MemberIter member_iter = pp::get_member_information(&test, i);
 
-    pp::TypeInfo<Foo>::base temp = {};
-
-    pp::TypeInfo<StringTest *> info = {};
-
-    for(int i = 0; (i < pp::TypeInfo<StringTest>::member_count); ++i) {
-        auto mi = pp::get_member_information(&test, i);
-
-        switch(mi.type) {
+        switch(member_iter.type) {
             case pp::Type_int: {
-                if(mi.arr) {
-                    for(int j = 0; (j < mi.arr); ++j) {
-                        int *integer = (int *)mi.ptr;
+                int &member = *(int *)member_iter.ptr;
+                ++member;
+            } break;
 
-                        printf("%d\n", integer[j]);
-                    }
-                } else {
-                    int integer = *(int *)mi.ptr;
-
-                    printf("%d\n", integer);
-                }
+            case pp::Type_Vector2d: {
+                Vector2d &member = *(Vector2d *)member_iter.ptr;
+                ++member;
             } break;
 
             case pp::Type_float: {
-                if(mi.is_ptr) {
-                    float *real = *(float **)mi.ptr;
-
-                    printf("%f\n", *real);
-                }
-            } break;
-
-            case pp::Type_char: {
-                char *str = *(char **)mi.ptr;
-
-                printf("%s\n", str);
+                float &member = *(float *)member_iter.ptr;
+                ++member;
             } break;
         }
     }
 
-    int i = 0;
+    std::cout << std::endl << "After";
+    pp::print(test);
 
-    //
-    //
-    //
+    /* The code will print the following:
+        "Before
+         Test test
+             int a = 1
+             int b = 2
+             float c = 3.500000
+             Vector2d d
+                 int x = 4
+                 int y = 5
 
+        After
+        Test test
+            int a = 2
+            int b = 3
+            float c = 4.500000
+            Vector2d d
+                int x = 5
+                int y = 6"*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-    {
-        pp::MemberDefinition *mems = pp::get_members_of_<StringTest>();
-        for(int i = 0; (i < pp::get_member_count<StringTest>()); ++i) {
-            pp::MemberDefinition cur = mems[i];
-
-            void *member_ptr = (size_t *)((char unsigned *)&test + cur.offset);
-            switch(cur.type) {
-                case pp::Type_int: {
-                    int integer = *(int *)member_ptr;
-
-                    int j = 0;
-                } break;
-
-                case pp::Type_char: {
-                    char *txt = *(char **)member_ptr;
-
-                    int j = 0;
-                } break;
-            }
-        }
-    }
-#endif
-    printf("\n");
     return(0);
 }
 
@@ -297,4 +294,39 @@ int main(int /*argc*/, char ** /*argv*/) {
 
 
 
+#if 0
+int i = 0;
+//
+//
+//
+{
+    pp::MemberDefinition *mems = pp::get_members_of_<StringTest>();
+    for(int i = 0; (i < pp::get_member_count<StringTest>()); ++i) {
+        pp::MemberDefinition cur = mems[i];
 
+        void *member_ptr = (size_t *)((char unsigned *)&test + cur.offset);
+        switch(cur.type) {
+            case pp::Type_int: {
+                int integer = *(int *)member_ptr;
+
+                int j = 0;
+            } break;
+
+            case pp::Type_char: {
+                char *txt = *(char **)member_ptr;
+
+                int j = 0;
+            } break;
+        }
+    }
+}
+printf("\n");
+return(0);
+}
+
+
+
+
+
+
+#endif
