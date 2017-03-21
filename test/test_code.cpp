749c1b03c15ef381ee319a104bd208fc6f8b3ebe
Jonathan Livingstone <seagull127@ymail.com>
2017-03-21 22:13:37 +0000

    fj

--- test/test_code.cpp
+++ test/test_code.cpp
@@ -1,5 +1,4 @@
 #include "pp_generated/test_code_generated.h"
-#include <string.h>
 
 class Shape {
 public:
@@ -13,7 +12,6 @@ public:
 };
 
 int main(int argc, char **argv) {
-
     Rectangle r;
     r.width = 10;
     r.height = 10;
